15f6197cb97fb42c76dc019536e960a03df81159
xioaheixi <justinzhao893@gmail.com>
2025-10-13 11:33:28 +1100

    Added toi gitignore.

--- GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
+++ GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192936408246c9aa0746fb1b99dec4414be08bf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6834dd6708c3d2a6db45981296b2e66eca2310e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
