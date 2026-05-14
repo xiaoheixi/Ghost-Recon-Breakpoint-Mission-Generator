06572eeb4d01bdd8f26acd1825ececcc22156b32
xioaheixi <justinzhao893@gmail.com>
2026-05-14 21:04:11 +1000

    SOAR Operations updated.

--- GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
+++ GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc14ebc9467e125b77e7479033c0c457545e27ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afac35dd7701c4c177b2a0c97243ea011cff8eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
