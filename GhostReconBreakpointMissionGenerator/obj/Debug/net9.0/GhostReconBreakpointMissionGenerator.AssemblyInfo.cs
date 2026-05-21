31426ac6ff7ee4f42e353fcee78057499bd5bc41
xioaheixi <justinzhao893@gmail.com>
2026-05-21 10:23:16 +1000

    Auroa Special Forces 1st SFG operations added!

--- GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
+++ GhostReconBreakpointMissionGenerator/obj/Debug/net9.0/GhostReconBreakpointMissionGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e85f02b58cb36044c2fccb230be2a0018303189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9046fe363aa91485a7b0f9cdc6d1de505712d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GhostReconBreakpointMissionGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
